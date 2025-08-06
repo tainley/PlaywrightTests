3237d0749a2925ff0651ab64c8c7d566c0d46f9c
Tim Ainley <Tim.Ainley@gmail.com>
2025-08-06 16:36:22 +0100

    still working

--- Steps/NavigationStepDefinitions.cs
+++ Steps/NavigationStepDefinitions.cs
@@ -5,19 +5,21 @@ using Reqnroll;
 namespace PlaywrightTests.Steps
 {
     [Binding]
-    public class NavigationStepDefinitions
+    public class NavigationStepDefinitions : IDisposable
     {
+        private readonly IPlaywright playwright;
+        private readonly IBrowser browser;
+        private readonly IPage page;
         private readonly HomePage homePage;
         private readonly InstallationPage installationPage;
 
         public NavigationStepDefinitions()
         {
-            IPlaywright playwright = Microsoft.Playwright.Playwright.CreateAsync().GetAwaiter().GetResult();
-            IBrowser browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true }).GetAwaiter().GetResult();
-            IPage page = browser.NewPageAsync().GetAwaiter().GetResult();
-
-            this.homePage = new HomePage(page);
-            this.installationPage = new InstallationPage(page);
+            playwright = Playwright.CreateAsync().GetAwaiter().GetResult();
+            browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true }).GetAwaiter().GetResult();
+            page = browser.NewPageAsync().GetAwaiter().GetResult();
+            homePage = new HomePage(page);
+            installationPage = new InstallationPage(page);
         }
 
         [Given("I am on the homepage")]
@@ -36,6 +38,13 @@ namespace PlaywrightTests.Steps
         public async Task ThenIShouldSeeTheInstallationPage()
         {
             await installationPage.AssertTitle();
+        }
+
+        public void Dispose()
+        {
+            page?.CloseAsync().GetAwaiter().GetResult();
+            browser?.CloseAsync().GetAwaiter().GetResult();
+            playwright?.Dispose();
         }
     }
 }
